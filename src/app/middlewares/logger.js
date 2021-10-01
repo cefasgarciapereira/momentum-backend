99328c7e574b09ab73f91be62f64bea9a677c1d0
Cefas Garcia Pereira <cefasgarciapereira@gmail.com.br>
2021-10-01 18:29:08 -0300

    [feat]: middleware to log endpoints

---
+++ src/app/middlewares/logger.js
@@ -0,0 +1,27 @@
+const Log = require('../models/log');
+
+module.exports = async (req, res, next) => {
+    const pathsToLog = ['/login']
+
+    if (!pathsToLog.includes(req.path)) {
+        return next();
+    }
+
+    const date = new Date();
+
+    const log = {
+        url: req.path,
+        method: req.method,
+        params: req.params,
+        body: req.body,
+        ip: req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress,
+        time: `${date.toLocaleDateString('pt-BR')} ${date.toLocaleTimeString('pt-BR')}`
+    }
+    try {
+        await Log.create(log)
+        return next();
+    } catch (err) {
+        console.log("Erro de log", err)
+        return next();
+    }
+}
